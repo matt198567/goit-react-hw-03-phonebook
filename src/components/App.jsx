2b0bf166d7bd1624a8e48a40fb2639f78b1f2731
Oleksii Bykovskyi <bikovskij@ukr.net>
2022-10-11 22:28:57 +0300

    fix prevstate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,19 +15,19 @@ export class App extends Component {
     filter: '',
   };
 
-  componentDidUpdate(prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('oбновилось поле contacts');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
 
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('oбновилось поле contacts');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
 
